af732d235c900279b8164ce01218bdb048ca6131
JCH95 <jhorger95@gmail.com>
2022-05-31 23:51:54 -0500

    changed index.js and added to About

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import coverImage from './assets/images/oceanography-building.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={coverImage} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
